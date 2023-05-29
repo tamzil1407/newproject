b71d598dbe54dca5c27231cc34dd95b3fcc9db78
tamzil1407 <134823004+tamzil1407@users.noreply.github.com>
2023-05-29 22:01:34 +0530

    Update TicketBookingDao.java
    HH

--- src/main/java/com/infotech/book/ticket/app/dao/TicketBookingDao.java
+++ src/main/java/com/infotech/book/ticket/app/dao/TicketBookingDao.java
@@ -1,9 +1,1 @@
-package com.infotech.book.ticket.app.dao;
 
-import org.springframework.data.repository.CrudRepository;
-
-import com.infotech.book.ticket.app.entities.Ticket;
-
-public interface TicketBookingDao extends CrudRepository<Ticket, Integer>{
-	Ticket findByEmail(String email);
-}
